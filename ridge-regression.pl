9d1a287b2a1589cfd49b27e32d3cb250e4ae0442
Zeno Gantner <zeno.gantner@gmail.com>
2011-05-10 12:17:30 +0200

    complete writing out prediction; some cleanup

--- ridge-regression.pl
+++ ridge-regression.pl
@@ -45,7 +45,7 @@ my $params = msolve($xtx, $xty);
 # compute RSS and RMSE
 if ($compute_fit) {
         my $num_instances = (dims $instances)[0];
-        
+
         my $pred = $params->transpose x $instances; # parentheses or OO notation are important here!!
         my $rss = sum(($pred - $targets) ** 2);
         my $rmse = sqrt($rss / $num_instances);
@@ -56,13 +56,13 @@ if ($compute_fit) {
 if ($test_file) {
         my ( $test_instances, $test_targets ) = convert_to_pdl(read_data($test_file));
         my $test_pred = $params->transpose x $test_instances;
-        
+
         if ($prediction_file) {
-                say $test_pred; # TODO write out
+                write_vector($test_pred, $prediction_file);
         }
         else {
-                my $num_test_instances = (dims $test_instances)[0];                
-                
+                my $num_test_instances = (dims $test_instances)[0];
+
                 my $test_rss  = sum(($test_pred - $targets) ** 2);
                 my $test_rmse = sqrt($test_rss / $num_test_instances);
                 say "RMSE $test_rmse";
@@ -71,48 +71,58 @@ if ($test_file) {
 
 sub convert_to_pdl {
         my ($data_ref, $num_features) = @_;
-               
+
         my $instances = zeros scalar @$data_ref, $num_features + 1;
         my $targets   = zeros scalar @$data_ref, 1;
-        
+
         for (my $i = 0; $i < scalar @$data_ref; $i++) {
                 my ($feature_value_ref, $target) = @{ $data_ref->[$i] };
 
                 $instances($i, 0) .= 1; # this is the bias term
                 $targets($i, 0) .= $target;
-                
+
                 foreach my $id (keys %$feature_value_ref) {
                         $instances($i, $id) .= $feature_value_ref->{$id};
                 }
         }
-        
+
         return ( $instances, $targets );
 }
 
 sub read_data {
         my ($training_file) = @_;
-        
+
         my @labeled_instances = ();
-        
+
         my $num_features = 0;
-        
+
         open my $fh, '<', $training_file;
         while (<$fh>) {
                 my $line = $_;
                 chomp $line;
-                
+
                 my @tokens = split /\s+/, $line;
                 my $label = shift @tokens;
- 
+
                 my %feature_value = map { split /:/ } @tokens;
                 $num_features = List::Util::max(keys %feature_value, $num_features);
-                
+
                 push @labeled_instances, [ \%feature_value, $label ];
         }
         close $fh;
-        
+
         return (\@labeled_instances, $num_features); # TODO named return
 }
+
+sub write_vector {
+        my ($vector, $filename) = @_;
+        open my $fh, '>', $filename;
+        foreach my $col (0 .. (dims $vector)[0] - 1) {
+                say $fh $vector->at($col, 0);
+        }
+        close $fh;
+}
+
 
 sub usage {
     my ($return_code) = @_;

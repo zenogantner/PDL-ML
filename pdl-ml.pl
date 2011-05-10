74501b6a6800559f02c53381da6295adc6008f0f
Zeno Gantner <zeno.gantner@gmail.com>
2011-05-10 15:36:07 +0200

    add logistic regression (not really complete yet)

--- pdl-ml.pl
+++ pdl-ml.pl
@@ -1,9 +1,19 @@
 #!/usr/bin/perl
 
-# ridge regression example
-# get example dataset via wget http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/regression/abalone
+# Machine learning examples
+
+# Get example datasets for regression and classification with
+# wget http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/regression/abalone
+# wget http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/heart_scale
+
 # (c) 2011 Zeno Gantner
 # License: GPL
+
+# TODO:
+#  - create evaluation and prediction subroutines
+#  - handle arbitrary two-class and multi-class problems
+#  - --verbose
+#  - SVM
 
 use strict;
 use warnings;
@@ -20,6 +30,8 @@ GetOptions(
 	   'help'              => \(my $help            = 0),
 	   'compute-fit'       => \(my $compute_fit     = 0),
 	   'shrinkage=f'       => \(my $shrinkage       = 0.0),
+	   'epsilon=f'         => \(my $epsilon         = 0.001),
+	   'model=s'           => \(my $model           = 'lin_reg'),
 	   'training-file=s'   => \(my $training_file   = ''),
 	   'test-file=s'       => \(my $test_file       = ''),
 	   'prediction-file=s' => \(my $prediction_file = ''),
@@ -27,16 +39,34 @@ GetOptions(
 
 usage(0) if $help;
 
-$shrinkage += 0; # workaround for PDL or Getopt::Long bug (?)
+my $mode;
+if ($model eq 'lin_reg') {
+        $mode = 'regression';
+}
+elsif ($model eq 'log_reg') {
+        $mode = 'classification';
+}
+else {
+        say "Unknown model type '$model'" ;
+        usage(-1);
+}
+
+$shrinkage += 0.0; # workaround for PDL or Getopt::Long bug (?)
 
 if ($training_file eq '') {
-        print STDERR "Please give --training-file=FILE";
+        say "Please give --training-file=FILE";
         usage(-1);
 }
 
 my ( $instances, $targets ) = convert_to_pdl(read_data($training_file));
 
-my $params = train_ridge_regression($instances, $targets, $shrinkage);
+my $params;
+if ($model eq 'lin_reg') {
+        $params = train_ridge_regression($instances, $targets);
+}
+elsif ($model eq 'log_reg') {
+        $params = irls($instances, $targets);
+}
 
 # compute RSS and RMSE
 if ($compute_fit) {
@@ -69,15 +99,54 @@ exit 0;
 
 # compute ridge regression parameters
 sub train_ridge_regression {
-        my ($instances, $targets, $shrinkage) = @_;
-        
+        my ($instances, $targets) = @_;
+
         my $xtx = $instances x transpose $instances;
         $xtx += $shrinkage x identity($xtx);
         my $xty = $instances x transpose $targets;
-        
+
         return  msolve($xtx, $xty);
 }
-        
+
+# compute logistic regression parameters using iteratively reweighted least squares (IRLS)
+sub irls {
+        # TODO add regularization
+        my ($instances, $targets) = @_;
+
+        my $num_instances = (dims $instances)[0];
+        my $num_features  = (dims $instances)[1];
+        my $params      = zeros(1, $num_features);
+        my $old_p       = ones (1, $num_instances);
+        my $delta;
+
+        do {
+                my $scores = $instances->transpose x $params;
+
+                my $p = 1 / (1 + exp(-1 x $scores));
+
+                my $w = $p * (1 - $p);
+
+                #my $w_diag = stretcher($w);
+                # ugly workaround
+                my $w_diag = zeros($num_instances, $num_instances);
+                for (my $i = 0; $i < $num_instances; $i++) {
+                        $w_diag($i, $i) .= $w(0, $i);
+                }
+
+                my $w_diag_inv = minv $w_diag;
+
+                my $z = $instances->transpose x $params + $w_diag_inv x ($targets->transpose - $p);
+                my $xtw = $instances x $w_diag;
+
+                $params = msolve( $xtw x $instances->transpose, $xtw x $z );
+
+                $delta = sum(abs($p - $old_p));
+                $old_p = $p->copy;
+        } while ($delta > $epsilon);
+
+        return  $params;
+}
+
 # convert Perl data structure to piddles
 sub convert_to_pdl {
         my ($data_ref, $num_features) = @_;
@@ -147,6 +216,7 @@ Perl Data Language ridge regression example
 usage: $PROGRAM_NAME [OPTIONS] [INPUT]
 
     --help                  display this usage information
+    --model=MODEL           MODEL can be lin_reg or log_reg
     --compute-fit           compute RSS and RMSE on training data
     --shrinkage=NUM         shrinkage (regularization) parameter for ridge regression
     --training-file=FILE    read training data from FILE

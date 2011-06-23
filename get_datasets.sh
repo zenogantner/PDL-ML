96b1a7d9c87e16f20c5e4618275c37c356941f4d
Zeno Gantner <zeno.gantner@gmail.com>
2011-06-23 16:58:39 +0200

    add dataset download script

---
+++ get_datasets.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Get example datasets
+wget http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/a9a
+wget http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/a9a.t
+
+wget http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/regression/eunite2001
+wget http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/regression/eunite2001.t

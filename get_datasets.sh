90175bb8ae014fe2c700a337a2db7133b3208c5b
Zeno Gantner <zeno.gantner@gmail.com>
2011-06-23 18:04:38 +0200

    move linear SVM methods into one program

--- get_datasets.sh
+++ get_datasets.sh
@@ -6,3 +6,14 @@ wget http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/a9a.t
 
 wget http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/regression/eunite2001
 wget http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/regression/eunite2001.t
+
+wget http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/leu.bz2
+wget http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/leu.t.bz2
+bunzip2 leu.bz2
+bunzip2 leu.t.bz2
+
+wget http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/svmguide1
+wget http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/svmguide1.t
+
+http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/svmguide3
+http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/svmguide3.t
--- svm-linear.pl
+++ svm-linear.pl
@@ -1,18 +1,12 @@
 #!/usr/bin/perl
 
-# SVM example - stochastic (sub)gradient descent for linear SVMs
-
-# Get example dataset with
-# wget http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/binary/heart_scale
+# SVM example - linear SVMs
 
 # (c) 2011 Zeno Gantner
 # License: GPL 3 or later
 
 # TODO
-#  - block gradient descent
-#  - learn rate schedule
-#  - use more PDL features to speed up computation
-#  - implement stopping criterion as in lecture slides
+#  - dual coordinate descent: handle bias properly
 
 use strict;
 use warnings;
@@ -28,18 +22,19 @@ use PDL::NiceSlice;
 GetOptions(
 	   'help'              => \(my $help            = 0),
 	   'verbose'           => \(my $verbose         = 0),
+           'method=s'          => \(my $method          = 'dualcd'),
 	   'compute-fit'       => \(my $compute_fit     = 0),
 	   'epsilon=f'         => \(my $epsilon         = 0.1),
 	   'training-file=s'   => \(my $training_file   = ''),
 	   'test-file=s'       => \(my $test_file       = ''),
 	   'prediction-file=s' => \(my $prediction_file = ''),
+	   'c=f'               => \(my $c               = 1),
 	   'regularization=f'  => \(my $regularization  = 0.01),
 	   'learn-rate=f'      => \(my $learn_rate      = 0.001),
 	   'num-iter=i'        => \(my $num_iter        = 10),
 	  ) or usage(-1);
 
 usage(0) if $help;
-
 
 if ($training_file eq '') {
         say "Please give --training-file=FILE";
@@ -51,7 +46,22 @@ my $num_instances = (dims $instances)[0];
 my $num_features  = (dims $instances)[1];
 
 # solve optimization problem
-my ($bias, $beta) = sgd($instances, $targets);
+my $bias = 0;
+my $beta;
+if ($method eq 'dualcd') {
+        $beta = coordinate_descent($instances, $targets);
+}
+elsif ($method eq 'sgd') {
+        ($bias, $beta) = sgd($instances, $targets);
+}
+elsif ($method eq 'pegasos') {
+        ($bias, $beta) = pegasos($instances, $targets);
+}
+else {
+        say "Unknown method: $method";
+        usage(-1);
+}
+
 # prepare prediction function
 my $predict = sub { return $bias + $beta x $_[0] >= 0 ? +1 : -1; };
 my $predict_several = sub {
@@ -94,7 +104,43 @@ if ($test_file) {
 
 exit 0;
 
-# solve primal optimization problem
+sub bounded_by {
+        my ($x, $a, $b) = @_;
+        
+        return $a if $x <= $a;
+        return $b if $x >= $b;
+        return $x;
+}
+
+# solve dual optimization problem by coordinate descent
+sub coordinate_descent {
+        my ($x, $y) = @_;
+
+        my $alpha = zeros($num_instances);
+        my $beta  = zeros($num_features);
+
+        my $changes_counter = 0;
+        do {
+                $changes_counter = 0;
+                foreach my $i (0 .. $num_instances - 1) { # TODO random order
+                        my $delta_alpha = bounded_by(
+                                                 (1 - $y($i) * $beta x $x($i)) / sum($x($i) * $x($i)), # TODO more elegant scalar product
+                                                 -$alpha($i),
+                                                 $c - $alpha($i),
+                                                  );
+                        
+                        $alpha($i) .= $alpha($i) + $delta_alpha;
+                        $beta = $beta + $delta_alpha * $y($i) * $x($i)->transpose;
+
+                        $changes_counter++ if abs($delta_alpha) > $epsilon;
+                }
+                say "$changes_counter changes" if $verbose;
+        } while $changes_counter;
+        
+        return $beta;
+}
+
+# solve primal optimization problem by stochastic gradient descent
 sub sgd {
         my ($x, $y) = @_;
 
@@ -116,12 +162,46 @@ sub sgd {
         return ($bias, $beta);
 }
 
+# solve primal optimization problem by Pegasos
+sub pegasos {
+        my ($x, $y) = @_;
+
+        my $bias = 0;
+        my $beta = zeros $num_features;
+        
+        for (my $i = 1; $i <= $num_iter * $num_instances; $i++) {
+                my $index = rand($num_instances - 1);
+
+                if ($y($index) * ($bias + $beta x $instances($index)) < 1) {
+                        # compute subgradient
+                        my $diff_beta = $y($index) * $instances($index)->transpose;
+                        my $diff_bias = $y($index);
+                        
+                        # determine step size
+                        my $step_size = 1 / ($regularization * $i);
+                      
+                        # perform update
+                        $beta = (1 - $step_size * $regularization) * $beta + $step_size * $diff_beta;
+                        $bias -= $step_size * $diff_bias;
+                        
+                        # rescale
+                        my $inv_scaling_factor = sqrt($regularization) * mnorm($beta);
+                        $beta = $beta / $inv_scaling_factor if $inv_scaling_factor > 1;
+                }
+        }
+
+        say $bias;
+        say $beta;
+
+        return ($bias, $beta);
+}
+
 # convert Perl data structure to piddles
 sub convert_to_pdl {
         my ($data_ref, $num_features) = @_;
 
         my $instances = zeros scalar @$data_ref, $num_features;
-        my $targets   = zeros scalar @$data_ref; # TODO handle multi-class/multi-label here
+        my $targets   = zeros scalar @$data_ref;
 
         for (my $i = 0; $i < scalar @$data_ref; $i++) {
                 my ($feature_value_ref, $target) = @{ $data_ref->[$i] };
@@ -162,7 +242,7 @@ sub read_data {
         }
         close $fh;
 
-        $num_features++; # take care of features starting index 0
+        $num_features++; # take care of features starting with index 0
 
         return (\@labeled_instances, $num_features); # TODO named return
 }
@@ -177,27 +257,28 @@ sub write_vector {
         close $fh;
 }
 
-
 sub usage {
     my ($return_code) = @_;
 
     print << "END";
 $PROGRAM_NAME
 
-Perl Data Language SVM example: stochastic subgradient descent for linear SVMs
+Perl Data Language SVM example: linear SVM
 
 usage: $PROGRAM_NAME [OPTIONS] [INPUT]
 
     --help                  display this usage information
     --verbose               show diagnostic/progress output
+    --solver=SOLVER         SOLVER is one of dualcd, sgd, or pegasos; default is dualcd
     --epsilon=NUM           set convergence sensitivity to NUM
     --compute-fit           compute error on training data
     --training-file=FILE    read training data from FILE
     --test-file=FILE        evaluate on FILE
     --prediction-file=FILE  write predictions for instances in the test file to FILE
-    --regularization=NUM    regularization parameter C
-    --learn-rate=NUM        
-    --num-iter=N            number of passes over the training data
+    --c=NUM                 complexity parameter C (dualcd)
+    --regularization=NUM    regularization parameter (sgd and pegasos)
+    --learn-rate=NUM        (sgd)
+    --num-iter=N            number of passes over the training data (for sgd and pegasos)
 END
     exit $return_code;
 }
